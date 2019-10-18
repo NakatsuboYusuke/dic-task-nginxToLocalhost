9fb60b15eef7aa4b23bd7a482d12e05379f03a8f
NakatsuboYusuke <>
2019-10-18 14:58:29 +0900

    HerokuのWebサーバーの設定をNginxにしました

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,16 @@
-rails_root = File.expand_path('../../', __FILE__)
+# For Heroku Env
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+worker_processes 4
+listen 'unix:///tmp/nginx.socket', backlog: 1024
+
+before_fork do |server,worker|
+  FileUtils.touch('/tmp/app-initialized')
+end
+
+# For Local Env
+=begin rails_root = File.expand_path('../../', __FILE__)
 
 worker_processes 2
 working_directory rails_root
@@ -7,4 +19,4 @@ listen "#{rails_root}/tmp/unicorn.sock"
 pid "#{rails_root}/tmp/unicorn.pid"
 
 stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
+stdout_path "#{rails_root}/log/unicorn.log" =end
